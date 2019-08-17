cae218eed427cfb9adfb4333fe6f6c09ed0f794b
dawn1 <dawn1@liout.com>
2019-08-17 18:07:10 +0800

    初始化homepage基本结构

---
+++ src/components/homepage/Homepage.vue
@@ -0,0 +1,25 @@
+<template>
+<!--    头部-->
+    <m-header></m-header>
+<!--    左侧菜单-->
+    <m-side-menu></m-side-menu>
+<!--中间内容-->
+    <m-content></m-content>
+</template>
+
+<script>
+    //1 引入
+    import MHeader from "./Header";
+    import MSideMenu from "./SideMenu";
+    import MContent from "./Content";
+    export default {
+        name: "Homepage",
+        components:{
+            MHeader,MSideMenu,MContent,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
